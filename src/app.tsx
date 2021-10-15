b2222edd7add7cdcedf74268e3d7f4e61bad277f
thomasjm <pyro777@gmail.com>
2021-10-14 18:53:41 -0600

    Remove older stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,4 @@
 
-console.log("Hello world!");
-
-class Foo {
-  constructor(public options: any) {
-    console.log("Got options", options);
-  }
-}
-
 const DataExplorer = import("@nteract/data-explorer");
 
 console.log("Got DataExplorer", DataExplorer);
-
-console.log("Foo", new Foo({42: "bar"}));
